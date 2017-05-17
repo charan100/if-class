ec6838b78794f2c9a72306c2bab3b419e28bf187
peddintisaicharan <charan.peddinti@gmail.com>
2017-05-16 21:38:06 -0700

    break with continue

---
+++ break.java
@@ -0,0 +1,17 @@
+class Continue 
+{  
+public static void main(String[] args)
+ {  
+            for(int i=1;i<=5;i++)
+			{    
+                    for(int j=1;j<=3;j++)
+					{    
+                        if(i==3&&j==3)
+						{    
+                            continue;    
+                        }    
+                        System.out.println(i+" "+j);    
+                    }    
+            }    
+}  
+}  
