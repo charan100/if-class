dfa477f3e59d9a8a5c08d5b3511469d9e8c2ff36
peddintisaicharan <charan.peddinti@gmail.com>
2017-06-21 03:33:12 -0400

    multiple objects

---
+++ mulobjt.java
@@ -0,0 +1,19 @@
+class Style
+	{  
+	int id;  
+	String name;  
+	}  
+		class TestStyle1
+	{  
+		public static void main(String args[])
+		{  
+		Style s1=new Style();  
+		Style s2=new Style();   
+		s1.id=10;  
+		s1.name="SAI";  
+		s2.id=11;  
+		s2.name="CHARAN";  
+		System.out.println(s1.id+" "+s1.name);  
+		System.out.println(s2.id+" "+s2.name);  
+		}  
+	} 
