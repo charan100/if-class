c97233cb64fd710ab24da1d2a490a659f5e511e5
peddintisaicharan <charan.peddinti@gmail.com>
2017-06-21 03:19:17 -0400

    objects

---
+++ objt.java
@@ -0,0 +1,14 @@
+class Style
+	{  
+	int id;  
+	String name;  
+	}  
+		class StyleConstants
+		{  
+		public static void main(String args[])
+		{  
+		Style s1=new Style();  
+		System.out.println(s1.id);  
+	System.out.println(s1.name);  
+		}  
+	}  
