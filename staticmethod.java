143853cc4eeabafe2ca318917766c4df3989e93b
peddintisaicharan <charan.peddinti@gmail.com>
2017-06-22 08:25:20 -0400

    staticmethod

---
+++ staticmethod.java
@@ -0,0 +1,34 @@
+
+class Student
+	{  
+		int rollno;  
+		String name;  
+		static String college = "svit";  
+       
+		static void change()
+		{  
+			college = "kmm";  
+		}  
+  
+		Student(int r, String n)
+		{  
+			rollno = r;  
+			name = n;  
+		}  
+  
+		void display ()
+		{
+			System.out.println(rollno+" "+name+" "+college);
+		}  
+  
+		public static void main(String args[])
+		{  
+			Student.change();  
+			Student s1 = new Student (1,"charan");  
+			Student s2 = new Student (2,"mahesh");  
+			Student s3 = new Student (3,"Sunny");  
+			s1.display();  
+			s2.display();  
+			s3.display();  
+		}  
+	}  
