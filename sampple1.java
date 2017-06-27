71866e9335b8e6d0aad0cbcbd52696968acbe265
peddintisaicharan <charan.peddinti@gmail.com>
2017-06-27 12:41:25 -0400

    basics

---
+++ sampple1.java
@@ -0,0 +1,25 @@
+class Simple{
+	public static void main(String[] args){
+		//float f=10.5f;
+		//int a=(int)f;
+		//int a=130;
+		short a=10;
+		short b=5;
+		a=(short)(a+b);
+		//int c=20;
+		//int min=(a<b)?a:b;
+		//boolean c=true;
+		//byte b=10;
+		//byte c=(byte)(a+b);
+		System.out.println(a);
+		//System.out.println(++a);
+		//System.out.println(a>b||a++>c);
+		//System.out.println(a);
+		//System.out.println(a-b);
+		//System.out.println(a*b);
+		//System.out.println(a/b);
+		//System.out.println(a%b);
+
+
+	}
+}
