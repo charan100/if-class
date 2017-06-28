2958d77cebfb073c5199d3b19f223a63e5885299
peddintisaicharan <charan.peddinti@gmail.com>
2017-06-28 10:19:48 -0400

    checking fibonacci series

---
+++ fibonacci.java
@@ -0,0 +1,13 @@
+class Fibonacci{
+	public static void main(String[] args){
+		int a1=0,a2=1,a3,i,count=10;
+		System.out.println(a1+" "+a2);
+		for(i=2;i<count;++i){
+			a3=a1+a2;
+			System.out.println(" "+a3);
+			a1 = a2;
+			a2 = a3;
+			//System.out.println(" "+a3);
+		}
+	}
+}
