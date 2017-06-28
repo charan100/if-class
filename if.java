2574bced0d0ca2c9b014a9a3649fd5f346f11090
peddintisaicharan <charan.peddinti@gmail.com>
2017-06-28 10:21:41 -0400

    if statements

--- if.java
+++ if.java
@@ -1,12 +1,34 @@
-class If
-{  
-public static void main(String[] args) 
-{  
-    int age=222;
-	
-    if(age>2222)
-	{  
-        System.out.print("Age is greater than 22");  
-    }  
-}  
+class If {  
+public static void main(String[] args) {  
+    //int age=28;
+	//if(age>18){
+	int a=115;
+	if(a<50){
+		System.out.println("fsil");
+	}
+	else if(a>=50&&a<60){
+		System.out.println("d grade");
+	}
+	else if(a>=60&&a<70){
+		System.out.println("c grade");
+	}
+	else if(a>=70&&a<80){
+		System.out.println("b grade");
+	}
+	else if(a>=80&&a<90){
+		System.out.println("a grade");
+	}
+	else if(a>=90&&a<100){
+	System.out.println("a+ grade");
+	}
+	else{
+		System.out.println("invaild");
+	}
+	//if(a%2==0){
+		//System.out.println("given num is even");
+	}
+	//else{
+		//System.out.println("given num is odd");
+	//System.out.println("eligible to marriage");
+	//}
 }
