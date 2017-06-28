621580140a931bd6dce0fa9e64e2d02f256a2815
peddintisaicharan <charan.peddinti@gmail.com>
2017-06-28 10:25:21 -0400

    while and do while

---
+++ while.java
@@ -0,0 +1,20 @@
+class While{
+	public static void main(String[] args){
+		int i=1;
+		do{
+			System.out.println("infinite loop");
+		}while(true);
+			//System.out.println(i);
+			//i++;
+		//}
+		//while(i<10);
+		//{
+			//System.out.println("iinfinite loop");
+		//}
+		//int i=4;
+		//while(i<=20){
+			//System.out.println(i);
+			//i++;
+		//}
+	}
+}
