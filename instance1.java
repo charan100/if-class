92e2f6677941f539aa9996e295ec3d1124781bd5
peddintisaicharan <charan.peddinti@gmail.com>
2017-07-03 22:22:16 -0400

    instance block comes in order

---
+++ instance1.java
@@ -0,0 +1,18 @@
+class A{
+	A(){
+		System.out.println("parent class constructor invoked");
+	}
+}
+class B extends A{
+	B(){
+		//super();
+		System.out.println("child class constructor invoked");
+	}
+	{
+		System.out.println("instance initializer block is invoked");
+	}
+	public static void main(String[] args){
+		B b = new B();
+		B b1 = new B();
+	}
+}
