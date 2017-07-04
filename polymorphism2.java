75114a5bb33abe400a7af5654426e7e54723ca26
peddintisaicharan <charan.peddinti@gmail.com>
2017-07-04 19:52:27 -0400

    real example runtime polymorphism

---
+++ polymorphism2.java
@@ -0,0 +1,24 @@
+class Animal{
+	void eat(){
+		System.out.println("eating..");
+	}
+}
+class Dog extends Animal {
+	void eat(){
+		System.out.println("eating fruits..");
+	}
+}
+class Babydog extends Animal {
+	void eat(){
+		System.out.println("drinking milk...");
+	}
+	public static void main(String[] args){
+		Animal a1,a2,a3;
+		a1 = new Animal();
+		a2 = new Dog();
+		a3 = new Babydog();
+		a1.eat();
+		a2.eat();
+		a3.eat();
+	}
+}
