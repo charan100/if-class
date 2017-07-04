59066790f07e2e56fa321124998dc85a53abfcd2
peddintisaicharan <charan.peddinti@gmail.com>
2017-07-03 22:19:57 -0400

    checking instance variable

---
+++ instance.java
@@ -0,0 +1,14 @@
+class Bike{
+	int speed;
+	Bike (){
+		super();
+		System.out.println("Constructor is invoked");
+	}
+	{
+		System.out.println("instance initilizer block invoked");
+	}
+	public static void main(String[] args){
+		Bike b1 = new Bike();
+		Bike b2 = new Bike();
+	}
+}
