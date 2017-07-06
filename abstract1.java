74b2e4906c8c96336ff509af0f60da96e5b1bfb0
peddintisaicharan <charan.peddinti@gmail.com>
2017-07-05 21:28:13 -0400

    real scenario

---
+++ abstract1.java
@@ -0,0 +1,19 @@
+abstract class Shape{
+	abstract void draw();
+}
+		class Rectangle extends Shape{
+			void draw(){
+				System.out.println("drawing rectangle...");
+			}
+		}
+		class Circle extends Shape{
+			void draw(){
+				System.out.println("drawing circle..");
+			}
+		}
+		class TestAbstract{
+			public static void main(String[] args){
+				Shape s = new Circle();
+				s.draw();
+			}
+		}
