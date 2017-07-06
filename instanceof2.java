0b08fe13e523569a814c39096084c745e01f470c
peddintisaicharan <charan.peddinti@gmail.com>
2017-07-05 21:07:23 -0400

    upcasting and downcasting

---
+++ instanceof2.java
@@ -0,0 +1,31 @@
+interface printable{
+	class A implements printable{
+		public void a(){
+			System.out.println("a method");
+		}
+	}
+	class B implements printable{
+		public void b(){
+			System.out.println("b method");
+		}
+	}
+	class Call{
+		void invoke(printable p){  //upcasting
+			if(p instanceof A){
+				A a = (A)p;
+				a.a();
+			}
+			if(p instanceof B){
+				B b = (B)p;
+				b.b();
+			}
+		}
+	}
+	class TestInstanceof{
+		public static void main(String[] args){
+			printable p = new B();
+			Call c = new Call();
+			c.invoke(p);
+		}
+	}
+}
