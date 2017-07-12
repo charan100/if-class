90b23e191acba6b536e7d3bf477a342e46b875b7
peddintisaicharan <charan.peddinti@gmail.com>
2017-07-12 00:08:41 -0400

    simple example

---
+++ array.java
@@ -0,0 +1,13 @@
+class Testarray{
+	public static void main(String[] args){
+		int a[] = new int[5]; //decalration and instantiation
+		a[0] = 10; //initializatioon
+		a[1] = 20;
+		a[2] = 70;
+		a[3] = 40;
+		a[4] = 50;
+		//printinig array
+		for(int i = 0;i < a.length;i++) //length in property of array
+		System.out.println(a[i]);
+	}
+}
