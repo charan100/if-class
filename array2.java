1c33e6c41d65b8774e5bd21a92e0ab8752bc0630
peddintisaicharan <charan.peddinti@gmail.com>
2017-07-12 00:11:52 -0400

    min number of array using array

---
+++ array2.java
@@ -0,0 +1,13 @@
+class Testarray{
+	static void min(int arr[]){
+		int min = arr[0];
+		for(int i = 1;i < arr.length;i++)
+			if(min > arr[i])
+				min = arr[i];
+			System.out.println(min);
+	}
+	public static void main(String[] args){
+		int a[] = {33,3,4,5,10};
+		min(a);
+	}
+}
