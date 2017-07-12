fbe5be2ffad2678c1bb3dc691f653cd403dd6328
peddintisaicharan <charan.peddinti@gmail.com>
2017-07-12 00:09:51 -0400

    declaration and initialization

---
+++ array1.java
@@ -0,0 +1,8 @@
+class Testarray{
+	public static void main(String[] args){
+		int a[] = {33,3,4};  //decalration,instantiation and initialization
+		//printing array
+		for(int i = 0;i < a.length;i++) //length is property of array
+		System.out.println(a[i]);
+	}
+}
