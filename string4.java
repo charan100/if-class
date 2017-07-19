6e88646136ce36157cc127c7309bf664213ab641
peddintisaicharan <charan.peddinti@gmail.com>
2017-07-19 09:57:31 -0400

    string with compareTo()

---
+++ string4.java
@@ -0,0 +1,10 @@
+class Stringcomparison{  
+	public static void main(String args[]){  
+		String s1="Sachin";  
+		String s2="Sachin";  
+		String s3="Ratan";  
+		System.out.println(s1.compareTo(s2));//0  
+		System.out.println(s1.compareTo(s3));//s1>s3  
+		System.out.println(s3.compareTo(s1));//s3 < s1  
+	}  
+}  
